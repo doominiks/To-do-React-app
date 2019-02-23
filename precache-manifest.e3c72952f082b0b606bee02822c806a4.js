93f6712738be50f2c2fd61f7588c080e165f4694
Dominik <dominik@MacBook-Pro-Arkadiusz.local>
2019-02-23 14:53:36 +0100

    Updates

---
+++ precache-manifest.e3c72952f082b0b606bee02822c806a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd9150829ebfa2c0306d",
+    "url": "/To-do-React-app/static/js/runtime~main.dd915082.js"
+  },
+  {
+    "revision": "ca1b0e2c29ed0c6a4cf0",
+    "url": "/To-do-React-app/static/js/main.ca1b0e2c.chunk.js"
+  },
+  {
+    "revision": "77a458057ff8f8a54ef7",
+    "url": "/To-do-React-app/static/js/1.77a45805.chunk.js"
+  },
+  {
+    "revision": "ca1b0e2c29ed0c6a4cf0",
+    "url": "/To-do-React-app/static/css/main.86aae6ea.chunk.css"
+  },
+  {
+    "revision": "b131447c0fed9aa016cfb464f8aa7d94",
+    "url": "/To-do-React-app/index.html"
+  }
+];
